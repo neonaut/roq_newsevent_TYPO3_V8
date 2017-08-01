20a6776631844141ca5be7d6a89bf0397f92caf0
Oliver Eglseder <oliver.eglseder@in2code.de>
2017-08-01 14:38:46 +0200

    [TASK] Remove migrated fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,10 +4,6 @@
 CREATE TABLE tx_news_domain_model_news (
   tx_roqnewsevent_is_event tinyint(1) unsigned DEFAULT '0' NOT NULL,
   tx_roqnewsevent_start int(11) DEFAULT '0' NOT NULL,
-  tx_roqnewsevent_startdate int(11) DEFAULT '0' NOT NULL,
-  tx_roqnewsevent_starttime int(11) DEFAULT '0' NOT NULL,
   tx_roqnewsevent_end int(11) DEFAULT '0' NOT NULL,
-  tx_roqnewsevent_enddate int(11) DEFAULT '0' NOT NULL,
-  tx_roqnewsevent_endtime int(11) DEFAULT '0' NOT NULL,
   tx_roqnewsevent_location varchar(255) DEFAULT '' NOT NULL,
 );
