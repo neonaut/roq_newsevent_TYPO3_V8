b96fdcf5a43e6d1c4ad99de19f020e21b835da06
Oliver Eglseder <oliver.eglseder@in2code.de>
2017-08-01 10:05:49 +0200

    [TASK] Move TS template registration to sys_template TCA

--- Configuration/TCA/sys_template.php
+++ Configuration/TCA/sys_template.php
@@ -1,6 +1,2 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('roq_newsevent', 'Configuration/TypoScript', 'News event');
