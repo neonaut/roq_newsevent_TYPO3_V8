8c8d32f63c6aafd3d5f5ad35467a0b0417f25cb7
Björn <b.heggemann@neonaut.de>
2018-06-26 12:41:05 +0200

    [TASK] Raise news dependencies to include version 7.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.11-8.7.99',
-            'news' => '5.0.0-6.0.99',
+            'news' => '6.0.0-7.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
