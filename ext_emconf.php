a2e97505a530f215293860711a8384fad11ab49f
Paul Golmann <p.golmann@neonaut.de>
2019-03-14 14:02:18 +0100

    [!!!][TASK] Change target to typo3 ^8.7, php ^7.0 and news ^7.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,20 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'News event (HF-MOD)',
+    'title' => 'News event for TYPO3 8/news 7',
     'description' => 'Event extension based on the versatile news system. Supplies additional event functionality to news records.',
     'category' => 'plugin',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'author' => 'ROQUIN B.V., Oliver Eglseder, Sacha P. Suter',
-    'author_email' => 'extensions@roquin.nl, oliver.eglseder@in2code.de, support@hausformat.com',
-    'author_company' => 'ROQUIN B.V., in2code GmbH, .hausformat',
-    'version' => '4.0.2',
+    'author' => 'ROQUIN B.V., Oliver Eglseder, Sacha P. Suter, Paul Golmann',
+    'author_email' => 'extensions@roquin.nl, oliver.eglseder@in2code.de, support@hausformat.com, p.golmann@neonaut.de',
+    'author_company' => 'ROQUIN B.V., in2code GmbH, .hausformat, Neonaut GmbH',
+    'version' => '7.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.00-8.7.99',
-            'news' => '6.0.0-7.0.99',
+            'news' => '7.0.0-7.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
